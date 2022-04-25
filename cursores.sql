c2927a1283c9f1048107c369557d7cb622c1a45a
Isabela Bianca <79585893+isabelabianca@users.noreply.github.com>
2022-04-25 11:05:26 -0300

    Create cursores.sql

---
+++ cursores.sql
@@ -0,0 +1,28 @@
+set serveroutput on;
+create table t_funcionario (
+    cd_fun      number(3),
+    nm_fun      varchar2(50),
+    salario     number(10,2),
+    dt_adm      date
+);
+
+begin
+    insert into t_funcionario values (1, 'Chris', 10000, to_date('17/04/2000', 'dd/mm/yyyy'));
+    insert into t_funcionario values (2, 'Drew', 16000, to_date('02/10/1998', 'dd/mm/yyyy'));
+    insert into t_funcionario values (3, 'Tonya', 5500, to_date('10/07/2010', 'dd/mm/yyyy'));   
+    insert into t_funcionario values (4, 'Julius', 7300, to_date('08/06/2015', 'dd/mm/yyyy'));
+    commit;
+end;
+
+declare 
+    cursor c_exibe is select nm_fun, salario from t_funcionario;
+    v_exibe c_exibe%rowtype;
+begin
+    open c_exibe;
+    loop
+        fetch c_exibe into v_exibe;
+    exit when c_exibe%notfound;
+    dbms_output.put_line('Nome: '||v_exibe.nm_fun||' - Salário: '||v_exibe.salario);
+    end loop;
+    close c_exibe;
+end;
